2fe47dbabe1d8581519792684119dc278cb0fe46
antoKeinanen <anto.keinanen@gmail.com>
2024-01-22 18:33:29 +0200

    make logo lead to the index page

--- components/logo/logo.tsx
+++ components/logo/logo.tsx
@@ -1,19 +1,22 @@
 import Image from "next/image";
 import styles from "./logo.module.css";
+import Link from "next/link";
 
 function Logo() {
   return (
-    <div className={styles.container}>
-      <Image
-        priority
-        className={styles.image}
-        src="/logo.png"
-        alt="Ai"
-        width={25}
-        height={25}
-      />
-      <h1 className={styles.text}>UrapolkuAI</h1>
-    </div>
+    <Link className={styles.link} href="/">
+      <div className={styles.container}>
+        <Image
+          priority
+          className={styles.image}
+          src="/logo.png"
+          alt="Ai"
+          width={25}
+          height={25}
+        />
+        <h1 className={styles.text}>UrapolkuAI</h1>
+      </div>
+    </Link>
   );
 }
 
