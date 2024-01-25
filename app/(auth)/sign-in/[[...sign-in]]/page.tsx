3399cd28545c5e9c10ab3ed949539e9a93c29088
antoKeinanen <anto.keinanen@gmail.com>
2024-01-25 19:07:12 +0200

    style login and sign up components to fit the brand better

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -4,7 +4,21 @@ import styles from "./page.module.css";
 export default function Page() {
   return (
     <div className={styles.container}>
-      <SignIn />
+      <SignIn
+        appearance={{
+          elements: {
+            card: styles.card,
+            socialButtons: styles.socialButtonsBlockButton,
+            otpCodeFieldInputs: styles.otpCodeFieldInputs,
+          },
+          variables: {
+            colorBackground: "#0b0121",
+            colorPrimary: "#bb9bff",
+            colorText: "#ececec",
+            colorTextOnPrimaryBackground: "#0b0121",
+          },
+        }}
+      />
     </div>
   );
 }
--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -4,7 +4,24 @@ import styles from "./page.module.css";
 export default function Page() {
   return (
     <div className={styles.container}>
-      <SignUp />
+      <SignUp
+        appearance={{
+          elements: {
+            card: styles.card,
+            socialButtons: styles.socialButtonsBlockButton,
+            selectButton: styles.selectButton,
+            selectOptionsContainer: styles.selectOptionsContainer,
+            selectOption__countryCode: styles.selectOptionCountryCode,
+            otpCodeFieldInputs: styles.otpCodeFieldInputs,
+          },
+          variables: {
+            colorBackground: "#0b0121",
+            colorPrimary: "#bb9bff",
+            colorText: "#ececec",
+            colorTextOnPrimaryBackground: "#0b0121",
+          },
+        }}
+      />
     </div>
   );
 }
